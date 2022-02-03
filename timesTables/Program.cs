57a7b96aaa26f04f9da51cae56bf9b1cb712c29f
Gabriel <gabriel@code.je>
2022-02-03 16:23:40 +0000

    initial app, with loop

---
+++ timesTables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace timesTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j <= 12; j++)
+                {
+                    Console.Out.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
