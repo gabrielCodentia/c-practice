759f66cdca4087028d839e00cd043372f0ede24e
Gabriel <gabriel@code.je>
2022-02-04 09:25:15 +0000

    fix indentation and delete empty lines

--- timesTables/Program.cs
+++ timesTables/Program.cs
@@ -10,18 +10,18 @@ namespace timesTables
             string userInput = Console.ReadLine();
             int number;
             bool success = int.TryParse(userInput, out number);
-            if(!success){
+            if(!success)
+            {
                 return;
             }
-            else {
+            else
+            {
                 for (int i = 0; i <= 12; i++)
                 {
                     for (int j = 0; j <= 12; j++)
                     {
-
                         if (number == j) {
-                        Console.Out.WriteLine($"{i} * {j} = {i * j}");
-
+                           Console.Out.WriteLine($"{i} * {j} = {i * j}");
                         }
                     }
                 }
