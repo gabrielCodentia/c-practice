07efdd626ab4f7b66f192868187dc0a7de11e834
Gabriel <gabriel@code.je>
2022-02-04 10:39:58 +0000

    cope with bad input

--- timesTables/Program.cs
+++ timesTables/Program.cs
@@ -8,7 +8,6 @@ namespace timesTables
         static void Main(string[] args)
         {
            TimesTable();
-           AskAgain();
         }
 
         static void TimesTable()
@@ -16,10 +15,11 @@ namespace timesTables
             Console.Out.WriteLine("Which times table you want at the start?");
             string userAnswer = Console.ReadLine();
             int number;
-            bool success = int.TryParse(userInput, out number);
-            if(!success)
+            bool success = int.TryParse(userAnswer, out number);
+            if(!success | number > 12 | number < 0)
             {
-                return;
+                Console.Out.WriteLine("Please use a number between 0 and 12");
+                TimesTable();
             }
             else
             {
@@ -27,11 +27,13 @@ namespace timesTables
                 {
                     for (int j = 0; j <= 12; j++)
                     {
-                        if (number == j) {
-                        Console.Out.WriteLine($"{i} * {j} = {i * j}");
+                        if (number == j)
+                        {
+                            Console.Out.WriteLine($"{i} * {j} = {i * j}");
                         }
                     }
                 }
+                AskAgain();
             }
         }
 
@@ -40,9 +42,16 @@ namespace timesTables
             Console.Out.WriteLine("Would you like to see another one?(Yes/No)");
             string secondUserAnswer = Console.ReadLine();
             string capitalizedAnswer = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(secondUserAnswer);
-            if (capitalizedAnswer == "Yes"){
+            if (capitalizedAnswer == "Yes")
+            {
                 TimesTable();
             }
+            else if (capitalizedAnswer != "No")
+            {
+              Console.Out.WriteLine("Please type Yes or No");
+              AskAgain();
+            }
+
         }
     }
 }
